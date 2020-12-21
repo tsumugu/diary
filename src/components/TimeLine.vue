006346906665010cf1908cec4d6494d32dd26355
山口紡 <tsumugu@Tsumugus-MacBook-Air.local>
2020-12-22 01:42:48 +0900

    feat: TLを日ごとに表示するように変更

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="timeline">
-    <div v-for="item in TLItemsList"><TLItem :propsItem=item /></div>
+    <div v-for="(posts, day) in TLItemsList">
+      <h1>{{day}}</h1>
+      <div v-for="post in posts">
+        <TLItem :propsItem=post />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 import axios from 'axios'
 import firebase from 'firebase'
 var database = firebase.database()
@@ -23,7 +29,9 @@ export default {
   data() {
     return {
       userInfo: null,
-      TLItemsList: [],
+      postsList: [],
+      postsOrderedbyDateList: [],
+      TLItemsList: {},
       PM: null,
       FM: null
     }
@@ -41,7 +49,7 @@ export default {
       this.FM = new FriendsManager(axios, database, this.userInfo)
       firebase.database().ref("posts/"+this.userInfo.uid).on('value', (snapshot) =>{
         var tlitems = snapshot.val()
-        var returnPostsList = []
+        var tlitemslength = Object.keys(tlitems).length
         Object.keys(tlitems).forEach(postid => {
           var itemObj = tlitems[postid]
           //nameを取得していく
@@ -63,12 +71,48 @@ export default {
                 "name": friendName
               }
             }
-            returnPostsList.push(returnObj)
+            this.postsList.push(returnObj)
+            // 全件処理が完了したら実行
+            if (tlitemslength == this.postsList.length) {
+              this.makePostsOrderedbyDateList()
+            }
           })
         })
-        this.TLItemsList = returnPostsList
-        console.log(this.TLItemsList)
       })
+    },
+    makePostsOrderedbyDateList() {
+      // 日ごとに
+      this.postsOrderedbyDateList = {}
+      this.postsList.forEach(post => {
+        //var tmpDayString = post.when.split("T")[0].split("-").join("/")
+        var tmpDayString = post.when.split("T")[0]
+        if (this.postsOrderedbyDateList[tmpDayString] == undefined) {
+          this.postsOrderedbyDateList[tmpDayString] = []
+        }
+        this.postsOrderedbyDateList[tmpDayString].push(post)
+      })
+      this.makeTLItemsList()
+    },
+    makeTLItemsList() {
+      var todayDate = moment().utcOffset("+0900").format("YYYY-MM-DD")
+      var posts = null
+      var postDate = null
+
+      var todayPosts = this.postsOrderedbyDateList[todayDate]
+      if (todayPosts != undefined) {
+        posts = todayPosts
+        postDate = todayDate
+      } else {
+        var latestPostDate = Object.keys(this.postsOrderedbyDateList)[0]
+        if (latestPostDate != undefined) {
+          posts = this.postsOrderedbyDateList[latestPostDate]
+          postDate = latestPostDate
+        }
+      }
+      this.TLItemsList = {}
+      this.TLItemsList[postDate] = posts
+      //.split("-").join("/")
+      console.log(this.TLItemsList)
     }
   },
   mounted() {
