13a9184b507f708efb9a5dd8da320307cc231a18
山口紡 <tsumugu1515@gmail.com>
2021-02-07 20:10:12 +0900

    feat: 未ログイン時のアクセス制限を実装

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -7,51 +8,61 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Home.vue')
+    component: () => import('../views/Home.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/regist',
     name: 'regist',
-    component: () => import('../views/RegistEventInfo.vue')
+    component: () => import('../views/RegistEventInfo.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/editpost/:postid',
     name: 'editpost',
-    component: () => import('../views/EditEventinfo.vue')
+    component: () => import('../views/EditEventinfo.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/user/:userid',
     name: 'useranalyze',
-    component: () => import('../views/UserAnalyze.vue')
+    component: () => import('../views/UserAnalyze.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/editfriends',
     name: 'editfriends',
-    component: () => import('../views/EditFriends.vue')
+    component: () => import('../views/EditFriends.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/postlist',
     name: 'editpostlist',
-    component: () => import('../views/EditPostList.vue')
+    component: () => import('../views/EditPostList.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/signup',
     name: 'signup',
-    component: () => import('../views/SignUp.vue')
+    component: () => import('../views/SignUp.vue'),
+    meta: { requiresAuth: false }
   },
   {
     path: '/signin',
     name: 'signin',
-    component: () => import('../views/SignIn.vue')
+    component: () => import('../views/SignIn.vue'),
+    meta: { requiresAuth: false }
   },
   {
     path: '/signout',
     name: 'signout',
-    component: () => import('../views/SignOut.vue')
+    component: () => import('../views/SignOut.vue'),
+    meta: { requiresAuth: false }
   },
   {
     path: '*',
-    component: () => import('../views/NotFoundComponent.vue')
+    component: () => import('../views/NotFoundComponent.vue'),
+    meta: { requiresAuth: false }
   }
 ]
 
@@ -59,6 +70,20 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next)=>{
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user != null) {
+        next()
+      } else {
+        next("/signin")
+      }
+    })
+  } else {
+    next()
+  }
 })
 
 export default router
